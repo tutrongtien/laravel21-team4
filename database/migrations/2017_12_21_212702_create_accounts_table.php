29b504428cca67dec572dcbfd40b5634dd015ef4
tutrongtien <tutrongtien.tt@gmail.com>
2017-12-22 04:53:56 +0700

    create migration

---
+++ database/migrations/2017_12_21_212702_create_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('account_number');
+            $table->integer('balance');
+            $table->integer('password_account');
+            $table->integer('type_account_id')->unsigned();
+            $table->foreign('type_account_id')->references('id')->on('type_accounts')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
